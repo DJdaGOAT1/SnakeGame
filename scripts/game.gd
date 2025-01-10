9de689e4ba7944f932bfc0eb29d38fb1c6c26fd3
DJdaGOAT1 <149193211+DJdaGOAT1@users.noreply.github.com>
2025-01-10 12:44:47 -0600

    test

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,13 @@
 extends Node
 
-# HIIIIIIIIIII 
+#game variables
+var score: int
+var gameStarted: bool = false
+
+#grid variables
+var cells: int = 30
+var cell_size: int = 50
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
